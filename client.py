a7fae8955f1d46f4bd971f49d6e60093546369f2
Rafael Marcelino de Carvalho <123484081+Rafae1040@users.noreply.github.com>
2023-05-02 16:35:54 -0300

    Update client.py

--- client.py
+++ client.py
@@ -5,26 +5,46 @@ import seaborn as sns
 
 tabela = pd.read_csv("telecom_users.csv")
 
+# 2 - Visualizar a Base de Dados
 
 tabela = tabela.drop('Unnamed: 0', axis=1)
 
 tabela.head()
 
+#   Entender quais informações estão disponíveis
+#   Descobrir as falhas na base de dados
 
+# 3 - Tratamentos de Dados 
+
+# Valores que estão reconhecidos de forma errada
 
 tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors="coerce")
 
+# Deletando as colunas vazias
 
 tabela = tabela.dropna(how="all", axis=1)
+
+# Deletando as linhas vazias
 
 tabela = tabela.dropna(how="any", axis=0)
 
 tabela.info()
 
+# 4 - Analise Inicial
+
+# Como estão os nossos cancelamentos?
+
+# Primeiro vamos renomear a coluna Churn para Cancelamento
+
 tabela = tabela.rename(columns={'Churn': 'Cancelamento'})
+
 
 tabela['Cancelamento'].value_counts()
 tabela['Cancelamento'].value_counts(normalize=True).map("{:.1%}".format)
+
+# 5 - Analise Completa
+
+# Comparar cada coluna da minha tabela com a coluna de cancelamento
 
 with sns.axes_style('whitegrid'):
 
